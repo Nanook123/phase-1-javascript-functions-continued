ea062b36e50cb7efa34cef556e34f3b6ffcc5ddd
Joshua Allford <joshallfordnanook@gmail.com>
2021-09-05 22:51:18 +0100

    commit tree 80e47b14eabbe92d85aaaaf89dd6b81b048f98e4

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Your code here
+
+function saturdayFun(activity = "roller-skate") {
+
+    return (`This Saturday, I want to ${activity}!`)
+
+
+}
+
+saturdayFun("bathe the dog")
+
+function mondayWork(work = `go to the office`) {
+    return `This Monday, I will ${work}.`
+}
+
+mondayWork(`work from home`)
+
+let wrapAdjective = function(star = "*"){
+    return function(adjective = "da-best"){
+        return `You are ${star}${adjective}${star}!`
+    }
+
+}
+wrapAdjective()(adjective)
+ 
